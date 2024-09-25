a42ecc34ad6e3e48693d206c230b3d07aa0e3ff2
Jeffrey Tan <i@jeffreytan.org>
2024-09-26 06:38:23 +0800

    Add files via upload

---
+++ yolov8_ros2/yolov8_ros2/object_detection_tf.py
@@ -0,0 +1,117 @@
+import rclpy
+from rclpy.node import Node
+from rclpy.qos import qos_profile_sensor_data
+from sensor_msgs.msg import Image, CameraInfo
+from geometry_msgs.msg import TransformStamped
+from message_filters import Subscriber, ApproximateTimeSynchronizer
+from tf2_ros import TransformBroadcaster
+
+import cv2
+from cv_bridge import CvBridge, CvBridgeError
+
+import numpy as np
+
+from ultralytics import YOLO
+
+
+class ObjectDetectionTF(Node):
+
+    def __init__(self, **args):
+        super().__init__('object_detection_tf')
+
+        self.target_name = 'cup'
+        self.frame_id = 'target'
+
+        self.detection_model = YOLO("yolov8m.pt")
+
+        self.bridge = CvBridge()
+
+        self.sub_info = Subscriber(
+            self, CameraInfo, '/camera/camera/aligned_depth_to_color/camera_info')
+        self.sub_color = Subscriber(
+            self, Image, '/camera/camera/color/image_raw')
+        self.sub_depth = Subscriber(
+            self, Image, '/camera/camera/aligned_depth_to_color/image_raw')
+        self.ts = ApproximateTimeSynchronizer(
+            [self.sub_info, self.sub_color, self.sub_depth], 10, 0.1)
+        self.ts.registerCallback(self.images_callback)
+        self.broadcaster = TransformBroadcaster(self)
+
+    def images_callback(self, msg_info, msg_color, msg_depth):
+        try:
+            img_color = CvBridge().imgmsg_to_cv2(msg_color, 'bgr8')
+            img_depth = CvBridge().imgmsg_to_cv2(msg_depth, 'passthrough')
+        except CvBridgeError as e:
+            self.get_logger().warn(str(e))
+            return
+
+        if img_color.shape[0:2] != img_depth.shape[0:2]:
+            self.get_logger().warn('カラーと深度の画像サイズが異なる')
+            return
+
+        detection_result = self.detection_model(img_color)
+        annotated_frame = detection_result[0].plot()
+
+        for r in detection_result:
+            boxes = r.boxes
+            for box in boxes:
+                b = box.xyxy[0].to('cpu').detach().numpy().copy()
+                c = box.cls
+                r.name = self.detection_model.names[int(c)]
+                r.u1 = int(b[0])
+                r.v1 = int(b[1])
+                r.u2 = int(b[2])
+                r.v2 = int(b[3])
+
+        cv2.imshow('color', annotated_frame)
+
+        target = None
+        for r in detection_result:
+            if r.name == self.target_name:
+                target = r
+                break
+
+        if target is not None:
+            u1 = round(target.u1)
+            u2 = round(target.u2)
+            v1 = round(target.v1)
+            v2 = round(target.v2)
+            u = round((target.u1 + target.u2) / 2)
+            v = round((target.v1 + target.v2) / 2)
+            depth = np.median(img_depth[v1:v2+1, u1:u2+1])
+            if depth != 0:
+                z = depth * 1e-3
+                fx = msg_info.k[0]
+                fy = msg_info.k[4]
+                cx = msg_info.k[2]
+                cy = msg_info.k[5]
+                x = z / fx * (u - cx)
+                y = z / fy * (v - cy)
+                self.get_logger().info(
+                    f'{target.name} ({x:.3f}, {y:.3f}, {z:.3f})')
+                ts = TransformStamped()
+                ts.header = msg_depth.header
+                ts.child_frame_id = self.frame_id
+                ts.transform.translation.x = x
+                ts.transform.translation.y = y
+                ts.transform.translation.z = z
+                self.broadcaster.sendTransform(ts)
+
+        img_depth *= 16
+        if target is not None:
+            pt1 = (int(target.u1), int(target.v1))
+            pt2 = (int(target.u2), int(target.v2))
+            cv2.rectangle(img_depth, pt1=pt1, pt2=pt2, color=0xffff)
+
+        cv2.imshow('depth', img_depth)
+        cv2.waitKey(1)
+
+
+def main():
+    rclpy.init()
+    object_detection_tf = ObjectDetectionTF()
+    try:
+        rclpy.spin(object_detection_tf)
+    except KeyboardInterrupt:
+        pass
+    rclpy.shutdown()
