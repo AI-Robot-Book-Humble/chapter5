a4fc4a09a023d24aba33980df94148097152bbd4
Jeffrey Tan <i@jeffreytan.org>
2024-10-08 21:20:25 +0800

    tidy up object_detection_tf.py

--- yolov8_ros2/yolov8_ros2/object_detection_tf.py
+++ yolov8_ros2/yolov8_ros2/object_detection_tf.py
@@ -1,6 +1,6 @@
 import rclpy
 from rclpy.node import Node
-from rclpy.qos import qos_profile_sensor_data
+from rclpy.callback_groups import ReentrantCallbackGroup
 from sensor_msgs.msg import Image, CameraInfo
 from geometry_msgs.msg import TransformStamped
 from message_filters import Subscriber, ApproximateTimeSynchronizer
@@ -8,7 +8,6 @@ from tf2_ros import TransformBroadcaster
 
 import cv2
 from cv_bridge import CvBridge, CvBridgeError
-
 import numpy as np
 
 from ultralytics import YOLO
@@ -16,26 +15,31 @@ from ultralytics import YOLO
 
 class ObjectDetectionTF(Node):
 
-    def __init__(self, **args):
+    def __init__(self):
         super().__init__('object_detection_tf')
 
-        self.target_name = 'cup'
-        self.frame_id = 'target'
+        self.target_name = 'cup'  # 探す物体名
+        self.frame_id = 'target'  # ブロードキャストするtfの名前
 
-        self.detection_model = YOLO("yolov8m.pt")
-
-        self.bridge = CvBridge()
-
+        # message_filtersを使って3個のトピックのサブスクライブをまとめて処理する．
+        self.callback_group = ReentrantCallbackGroup()   # コールバックの並行処理のため
         self.sub_info = Subscriber(
-            self, CameraInfo, '/camera/camera/aligned_depth_to_color/camera_info')
+            self, CameraInfo, '/camera/camera/aligned_depth_to_color/camera_info',
+            callback_group=self.callback_group)
         self.sub_color = Subscriber(
-            self, Image, '/camera/camera/color/image_raw')
+            self, Image, '/camera/camera/color/image_raw',
+            callback_group=self.callback_group)
         self.sub_depth = Subscriber(
-            self, Image, '/camera/camera/aligned_depth_to_color/image_raw')
+            self, Image, '/camera/camera/aligned_depth_to_color/image_raw',
+            callback_group=self.callback_group)
         self.ts = ApproximateTimeSynchronizer(
             [self.sub_info, self.sub_color, self.sub_depth], 10, 0.1)
         self.ts.registerCallback(self.images_callback)
+        
+        # 認識した物体の位置をtfとして出力するためのブロードキャスタ
         self.broadcaster = TransformBroadcaster(self)
+
+        self.detection_model = YOLO("yolov8m.pt")
 
     def images_callback(self, msg_info, msg_color, msg_depth):
         try:
@@ -49,38 +53,49 @@ class ObjectDetectionTF(Node):
             self.get_logger().warn('カラーと深度の画像サイズが異なる')
             return
 
-        detection_result = self.detection_model(img_color)
-        annotated_frame = detection_result[0].plot()
+        if img_depth.dtype == np.uint16:
+            depth_scale = 1e-3
+            img_depth_conversion = True
+        elif img_depth.dtype == np.float32:
+            depth_scale = 1
+            img_depth_conversion = False
+        else:
+            self.get_logger().warn('深度画像の型に対応していない')
+            return
+        
+        # 物体認識
+        boxes = []
+        classes = []
+        results = self.detection_model(img_color, verbose=False)
+        names = results[0].names
+        boxes = results[0].boxes
+        classes = results[0].boxes.cls
+        img_color = results[0].plot()
 
-        for r in detection_result:
-            boxes = r.boxes
-            for box in boxes:
-                b = box.xyxy[0].to('cpu').detach().numpy().copy()
-                c = box.cls
-                r.names = self.detection_model.names[int(c)]
-                r.u1 = int(b[0])
-                r.v1 = int(b[1])
-                r.u2 = int(b[2])
-                r.v2 = int(b[3])
+        cv2.imshow('color', img_color)
 
-        cv2.imshow('color', annotated_frame)
-
-        target = None
-        for r in detection_result:
-            if r.names == self.target_name:
-                target = r
+        # 物体に認識の結果に指定された名前があるか調べる．
+        box = None
+        for b, c in zip(boxes, classes):
+            if names[int(c)] == self.target_name:
+                box = b
                 break
 
-        if target is not None:
-            u1 = round(target.u1)
-            u2 = round(target.u2)
-            v1 = round(target.v1)
-            v2 = round(target.v2)
-            u = round((target.u1 + target.u2) / 2)
-            v = round((target.v1 + target.v2) / 2)
+        # カラー画像内で検出された場合は，深度画像から3次元位置を算出．
+        depth = 0
+        (bu1, bu2, bv1, bv2) = (0, 0, 0, 0)
+        if box is not None:
+            a = 0.5
+            bu1, bv1, bu2, bv2 = [int(i) for i in box.xyxy.cpu().numpy()[0]]
+            u1 = round((bu1 + bu2) / 2 - (bu2 - bu1) * a / 2)
+            u2 = round((bu1 + bu2) / 2 + (bu2 - bu1) * a / 2)
+            v1 = round((bv1 + bv2) / 2 - (bv2 - bv1) * a / 2)
+            v2 = round((bv1 + bv2) / 2 + (bv2 - bv1) * a / 2)
+            u = round((bu1 + bu2) / 2)
+            v = round((bv1 + bv2) / 2)
             depth = np.median(img_depth[v1:v2+1, u1:u2+1])
             if depth != 0:
-                z = depth * 1e-3
+                z = float(depth) * depth_scale
                 fx = msg_info.k[0]
                 fy = msg_info.k[4]
                 cx = msg_info.k[2]
@@ -88,7 +103,8 @@ class ObjectDetectionTF(Node):
                 x = z / fx * (u - cx)
                 y = z / fy * (v - cy)
                 self.get_logger().info(
-                    f'{target.names} ({x:.3f}, {y:.3f}, {z:.3f})')
+                    f'{self.target_name} ({x:.3f}, {y:.3f}, {z:.3f})')
+                # tfの送出
                 ts = TransformStamped()
                 ts.header = msg_depth.header
                 ts.child_frame_id = self.frame_id
@@ -97,10 +113,12 @@ class ObjectDetectionTF(Node):
                 ts.transform.translation.z = z
                 self.broadcaster.sendTransform(ts)
 
-        img_depth *= 16
-        if target is not None:
-            pt1 = (int(target.u1), int(target.v1))
-            pt2 = (int(target.u2), int(target.v2))
+        # 深度画像の加工
+        if img_depth_conversion:
+            img_depth *= 16
+        if depth != 0:  # 認識していて，かつ，距離が得られた場合
+            pt1 = (int(bu1), int(bv1))
+            pt2 = (int(bu2), int(bv2))
             cv2.rectangle(img_depth, pt1=pt1, pt2=pt2, color=0xffff)
 
         cv2.imshow('depth', img_depth)
